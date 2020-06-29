f0d4e3a8d4fd64ba2993b977dc8ca765c6b62ede
HerrysonNeubaner <herrysonlibrary@gmail.com>
2020-06-28 22:57:44 -0300

    Criação das animações de Scroll

--- js/scripts.js
+++ js/scripts.js
@@ -8,4 +8,39 @@ $(document).ready(function () {
 
     }, 250);
 
+    // Scrool para Seções
+    let navBtn = $('.nav-item');
+
+    let bannerSection = $('#mainSlider');
+    let aboutSection = $('#about-area');
+    let servicesSection = $('#services-area');
+    let applySection = $('#apply-area');
+    let contactSection = $('#contact-area');
+
+    let scrollTo = '';
+
+    $(navBtn).click(function () {
+
+        let btnID = $(this).attr('id');
+
+        console.log(btnID);
+
+        if (btnID == 'ist-menu') {
+            scrollTo = aboutSection;
+        } else if (btnID == 'service-menu') {
+            scrollTo = servicesSection;
+        } else if (btnID == 'contact-menu') {
+            scrollTo = contactSection;
+        } else {
+            scrollTo = bannerSection
+        }
+
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(scrollTo).offset().top - 70
+        }, 1500);
+
+    });
+
+
+
 });
